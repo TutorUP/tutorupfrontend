55fa0d4b5173d2b8153c13d759715876463bb44a
baldwina18 <baldwina18@up.edu>
2018-04-15 20:39:25 -0700

    Add subjects

--- src/app/create-tutor-page/create-tutor-page.component.ts
+++ src/app/create-tutor-page/create-tutor-page.component.ts
@@ -9,10 +9,28 @@ import { TutorService } from '../tutor.service';
 export class CreateTutorPageComponent implements OnInit {
 
   tutorService: TutorService = new TutorService();
+  courses: string[] = [];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addCourse()
+  {
+  	this.courses.push('');
+  }
+
+  getCourseLetters(): string[] 
+  {
+  	return [ 'BUS', 'CHM', 'COM', 'CS', 'CE','ME','EE','MTH','SOC','HST',
+  			 'ENG', 'SPA', 'FRN', 'BME', 'EGR','FA', 'THE', 'PHY', 'PHL'];
+  }
+
+  getCourseNumbers(): string[] 
+  {
+  	return [ '101', '110', '301', '305', '202','205','300','325','350','150',
+  			 '360', '325', '400', '425', '490','500', '525', '590', '595'];
   }
 
 }
